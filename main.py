a988aa36408d2b7670d174e784426240a4b12127
shivanand1 <96418666+shivanandsc@users.noreply.github.com>
2022-03-07 15:21:19 +0000

    psycopg2-binar added

--- main.py
+++ main.py
@@ -12,12 +12,13 @@ from wtforms import StringField, SubmitField,SelectField
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
 from functools import wraps
+import os
 print("hi")
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -30,7 +31,7 @@ def user_loader(user_id):
 
     return User.query.get(user_id)
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
